625e7b2eb2abbf6f9e3843905259437b076307fa
ihoro-epam <ihor_ostapovych@epam.com>
2022-11-22 11:16:13 +0200

    test(StudentsRepo): tests fixed

--- internal/core/repository/mongodb/students_test.go
+++ internal/core/repository/mongodb/students_test.go
@@ -9,42 +9,43 @@ import (
 	"testing"
 )
 
+type key string
+
+var k = key("withErr")
+
 type collectionMock struct {
 	data interface{}
 }
 
 func (m *collectionMock) InsertOne(ctx context.Context, document interface{},
 	_ ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
-	if ctx.Value("withErr") == true {
+	if ctx.Value(k) == true {
 		return &mongo.InsertOneResult{InsertedID: 0}, mongo.ErrNilDocument
 	}
 	m.data = document
 	return &mongo.InsertOneResult{InsertedID: 1}, nil
 }
 
-type test struct {
-	name    string
-	args    args
-	want    interface{}
-	wantErr bool
-}
+func TestStudentsRepo_SaveRSS(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		student domain.StudentRSS
+	}
+	type test struct {
+		name    string
+		args    args
+		want    domain.StudentRecord
+		wantErr bool
+	}
 
-type args struct {
-	ctx     context.Context
-	student domain.StudentRSS
-}
-
-func getTestCases(source string) []test {
-	type key string
-	k := key("withErr")
-	return []test{
+	tests := []test{
 		{
 			"no error",
 			args{
 				context.WithValue(context.Background(), k, false),
 				domain.StudentRSS{},
 			},
-			domain.StudentRecord{Source: source},
+			domain.StudentRecord{Source: domain.RSS},
 			false,
 		}, {
 			"with error",
@@ -52,25 +53,24 @@ func getTestCases(source string) []test {
 				context.WithValue(context.Background(), k, true),
 				domain.StudentRSS{},
 			},
-			domain.StudentRecord{Source: source},
+			domain.StudentRecord{},
 			true,
 		},
 	}
-}
-
-func TestStudentsRepo_SaveRSS(t *testing.T) {
-	tests := getTestCases(domain.RSS)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mock := collectionMock{}
 			repo := newRepo(&mock)
 
 			_, err := repo.SaveRSS(tt.args.ctx, tt.args.student)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("SaveRSS() error = %v, wantErr %v", err, tt.wantErr)
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("SaveRss() error expected but got %v", err)
+				}
 				return
 			}
-			if reflect.DeepEqual(mock.data, tt.want) {
+
+			if !reflect.DeepEqual(mock.data, tt.want) {
 				t.Errorf("SaveRSS() got = %v, want %v", mock.data, tt.want)
 			}
 		})
@@ -78,18 +78,49 @@ func TestStudentsRepo_SaveRSS(t *testing.T) {
 }
 
 func TestStudentsRepo_SaveWAC(t *testing.T) {
-	tests := getTestCases(domain.WAC)
+	type args struct {
+		ctx     context.Context
+		student domain.StudentWAC
+	}
+	type test struct {
+		name    string
+		args    args
+		want    domain.StudentRecord
+		wantErr bool
+	}
+	tests := []test{
+		{
+			"no error",
+			args{
+				context.WithValue(context.Background(), k, false),
+				domain.StudentWAC{},
+			},
+			domain.StudentRecord{Source: domain.WAC},
+			false,
+		}, {
+			"with error",
+			args{
+				context.WithValue(context.Background(), k, true),
+				domain.StudentWAC{},
+			},
+			domain.StudentRecord{},
+			true,
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mock := collectionMock{}
 			repo := newRepo(&mock)
 
-			_, err := repo.SaveRSS(tt.args.ctx, tt.args.student)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("SaveWAC() error = %v, wantErr %v", err, tt.wantErr)
+			_, err := repo.SaveWAC(tt.args.ctx, tt.args.student)
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("SaveWAC() error expected but got %v", err)
+				}
 				return
 			}
-			if reflect.DeepEqual(mock.data, tt.want) {
+
+			if !reflect.DeepEqual(mock.data, tt.want) {
 				t.Errorf("SaveWAC() got = %v, want %v", mock.data, tt.want)
 			}
 		})
