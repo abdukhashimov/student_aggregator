d75bc977b8fd61a4b0e8fadac6aeb76c88d3466b
madiyor-abdukhashimov <madiyor.abdukhashimov@global-fashion-group.com>
2022-11-29 11:41:48 +0500

    add content-type as options

--- internal/core/ports/storage.go
+++ internal/core/ports/storage.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"io"
 
+	"github.com/abdukhashimov/student_aggregator/internal/core/domain"
 	"github.com/minio/minio-go/v7"
 )
 
 type StorageService interface {
 	SetClient(cl *minio.Client)
-	PutFile(ctx context.Context, objectName string, body io.Reader, size int64) (stug string, err error)
+	PutFile(ctx context.Context, options domain.PutFileOptions) (stug string, err error)
 	GetFile(ctx context.Context, slug string) (io.Reader, int64, error)
 }
