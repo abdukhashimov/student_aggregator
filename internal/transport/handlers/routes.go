9b8d5412133b62d2b497e23e435f8d749e80b9bf
Maksym Soroka <ms.mikro.1@gmail.com>
2022-12-01 12:26:25 +0200

    fix(handlers): simplify entity handlers

--- internal/transport/handlers/routes.go
+++ internal/transport/handlers/routes.go
@@ -12,21 +12,21 @@ func (s *Server) routes() {
 
 	noAuth := apiRouter.PathPrefix("").Subrouter()
 	{
-		noAuth.Handle("/health", healthCheck()).Methods(http.MethodGet)
-		noAuth.Handle("/users/login", s.loginUser()).Methods(http.MethodPost)
-		noAuth.Handle("/users", s.createUser()).Methods(http.MethodPost)
-		noAuth.Handle("/auth/refresh", s.refreshToken()).Methods(http.MethodPost)
+		noAuth.Handle("/health", http.HandlerFunc(healthCheck)).Methods(http.MethodGet)
+		noAuth.Handle("/users/login", http.HandlerFunc(s.loginUser)).Methods(http.MethodPost)
+		noAuth.Handle("/users", http.HandlerFunc(s.createUser)).Methods(http.MethodPost)
+		noAuth.Handle("/auth/refresh", http.HandlerFunc(s.refreshToken)).Methods(http.MethodPost)
 	}
 
 	authApiRoutes := apiRouter.PathPrefix("").Subrouter()
 	authApiRoutes.Use(s.authenticate)
 	{
-		authApiRoutes.Handle("/user", s.getCurrentUser()).Methods(http.MethodGet)
-		authApiRoutes.Handle("/schemas", s.createSchema()).Methods(http.MethodPost)
-		authApiRoutes.Handle("/schemas", s.listSchemas()).Methods(http.MethodGet)
-		authApiRoutes.Handle("/schemas/{id}", s.getSchemaById()).Methods(http.MethodGet)
-		authApiRoutes.Handle("/schemas/{id}", s.updateSchema()).Methods(http.MethodPatch)
-		authApiRoutes.Handle("/schemas/{id}", s.deleteSchema()).Methods(http.MethodDelete)
+		authApiRoutes.Handle("/user", http.HandlerFunc(s.getCurrentUser)).Methods(http.MethodGet)
+		authApiRoutes.Handle("/schemas", http.HandlerFunc(s.createSchema)).Methods(http.MethodPost)
+		authApiRoutes.Handle("/schemas", http.HandlerFunc(s.listSchemas)).Methods(http.MethodGet)
+		authApiRoutes.Handle("/schemas/{id}", http.HandlerFunc(s.getSchemaById)).Methods(http.MethodGet)
+		authApiRoutes.Handle("/schemas/{id}", http.HandlerFunc(s.updateSchema)).Methods(http.MethodPatch)
+		authApiRoutes.Handle("/schemas/{id}", http.HandlerFunc(s.deleteSchema)).Methods(http.MethodDelete)
 		authApiRoutes.Handle("/storage/upload", s.blobUpload()).Methods(http.MethodPost)
 	}
 }
@@ -39,13 +39,11 @@ func (s *Server) routes() {
 // @Accept json
 // @Produce json
 // @Router /health [get]
-func healthCheck() http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		resp := M{
-			"status":  "available",
-			"message": "healthy",
-			"data":    M{"hello": "world"},
-		}
-		writeJSON(rw, http.StatusOK, resp)
+func healthCheck(rw http.ResponseWriter, r *http.Request) {
+	resp := M{
+		"status":  "available",
+		"message": "healthy",
+		"data":    M{"hello": "world"},
 	}
+	writeJSON(rw, http.StatusOK, resp)
 }
